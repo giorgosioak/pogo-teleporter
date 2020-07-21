670fb947ec9991632a887d500cc2e3a5f3201f61
George Ioakeimidis <giorgosioak95@gmail.com>
2020-07-21 18:06:01 +0300

    added back command

--- teleporter.py
+++ teleporter.py
@@ -50,17 +50,22 @@ def yellowprint(text):
 def main(argv):
     """Main fuction."""
     lc = [200.0 , 200.0] # last coords
+    blc = lc # before last coords
     
     # read line from input
     for line in sys.stdin:
         regex = re.search(COORD_PATTERN, line)
         matched = regex.group(0) if regex else ""
-        if matched == "":
+        if matched == "" and "back" not in line:
             print('\033[91m' + "× No coords match! Skipping..." + '\033[0m')
             continue
-        line = matched.split(',')
-        lat  = str(line[0].strip())
-        lon = str(line[1].strip())
+        elif matched == "" and "back" in line:
+            lat = str(blc[0])
+            lon = str(blc[1])
+        else:
+            line = matched.split(',')
+            lat  = str(line[0].strip())
+            lon = str(line[1].strip())
 
         # Setup teleport command
         cmd = "adb.exe" if platform.system() == "Windows" else "adb"
@@ -77,14 +82,15 @@ def main(argv):
 
         if 200.0 in lc:
             print('\033[93m' + "» Teleporting..." + '\033[0m')
+            lc = [lat, lon] # for back command
         elif "2h" in cd:
             print('\033[93m' + "» Teleporting... Distance: " + '\033[94m' + '\033[1m' + str(round(dist,2)) + "km" +  '\033[0m' + '\033[93m' + " Cooldown: " + '\033[92m' + '\033[1m' + cd + '\033[0m')
         else:
             print('\033[93m' + "» Teleporting... Distance: " + '\033[94m' + '\033[1m' + str(round(dist,2)) + "km" +  '\033[0m' + '\033[93m' + " Cooldown: " + '\033[92m' + '\033[1m' + cd + "~" + safe + '\033[0m')
 
         # Save last coords
+        blc = lc
         lc = [lat, lon]
-
 #End
 
 # CALL MAIN
