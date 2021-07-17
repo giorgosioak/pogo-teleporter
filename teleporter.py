74ee70e1bcfcea865fcd10e44f1d9e8650d42b43
George Ioakeimidis <giorgosioak95@gmail.com>
2021-07-17 15:40:59 +0300

    Better distance messages
    * Added intro message
    * New cooldown times from pokexperience
    * Improved print

--- teleporter.py
+++ teleporter.py
@@ -9,8 +9,12 @@ import platform
 import re
 import sys
 from math import sin, cos, sqrt, atan2, radians
+from time import time
 
 COORD_PATTERN = r"-?(\d{1,3})\.(\d{1,15})[ ]?,[ ]?-?(\d{1,3})\.(\d{1,15})"
+pc = [0.0 , 0.0] # previous coords
+lc = [0.0 , 0.0] # latest coords
+# TODO: tp_time = time() # time since last teleport
 
 def get_distance(lat1, lon1, lat2, lon2):
     """Return distance of two coordinates."""
@@ -35,12 +39,15 @@ def get_distance(lat1, lon1, lat2, lon2):
 
 def get_cooldown(dist):
     """Return cooldown time of given distance."""
-    km = [0,1,5,10,25,30,65,81,100,250,500,750,1000,1500]
-    cd = ["0s","30s","2m","6m","11m","14m","22m","25m","35m","45m","1h","1.3h","1.5h","2h","2h"]
-    if dist > 1500:
-        return "2h", "2h"
+    km = [0,1,2,4,10,12,15,20,25,30,40,45,60,80,100,125,140,180,200,250,300,350,400,500,600,750,800,900,950,1000,1150,1200,1250,1266]
+    cd = ["0s","1m","2m","8m","9m","11m","13m","15m","18m","22m","23m","25m","27m","30m","33m","34m","36m","39m","42m","46m","50m","53m","56m","64m","72m", "82m", "86m", "93m", "97m", "100m", "111m", "115m", "118m", "120m"]
+    
+    if dist > km[-1]:
+        return "120m", "120m"
     for i,v in enumerate(km):
+        # passed distance
         if v > dist:
+            # return cd, safe times
             return cd[i-1], cd[i]
 
 def yellowprint(text):
@@ -49,9 +56,17 @@ def yellowprint(text):
 
 def main(argv):
     """Main fuction."""
-    lc = [200.0 , 200.0] # last coords
-    blc = lc # before last coords
-    
+
+    global pc # previous coords
+    global lc # latest coords
+    # TODO: global tp_time # time since last teleport
+
+    # print description message at start
+    print("\033[94m~=~=~ ~=~=~ ~=~=~ ~=~=~ ~=~=~ ~=~=~ ~=~=~ ~=~=~\033[0m")
+    print(" Pokemon Go Teleporter by \033[96m@giorgosioak\033[0m")
+    print(" Enter coords from links or text to teleport")
+    print("\033[94m~=~=~ ~=~=~ ~=~=~ ~=~=~ ~=~=~ ~=~=~ ~=~=~ ~=~=~\033[0m")
+
     # read line from input
     for line in sys.stdin:
         regex = re.search(COORD_PATTERN, line)
@@ -62,8 +77,8 @@ def main(argv):
             lat  = str(line[0].strip())
             lon = str(line[1].strip())
         elif "back" in line:
-            lat = str(blc[0])
-            lon = str(blc[1])
+            lat = str(pc[0])
+            lon = str(pc[1])
         elif any(x in line for x in ["quit", "exit"]):
             print('\033[94m' + "• Exiting..." + '\033[0m')
             exit()
@@ -84,17 +99,19 @@ def main(argv):
         cd,safe = get_cooldown(dist)
         
 
-        if 200.0 in lc:
+        if 0.0 in lc:
             print('\033[93m' + "» Teleporting..." + '\033[0m')
             lc = [lat, lon] # for back command
-        elif "2h" in cd:
+        elif dist == 0:
+            print('\033[93m' + "» Teleporting... Distance: " + '\033[94m' + '\033[1m' + str(round(dist,2)) + "km" +  '\033[0m' + '\033[93m' + " Cooldown: " + '\033[92m' + '\033[1m' + cd + '\033[0m')
+        elif "120m" in cd:
             print('\033[93m' + "» Teleporting... Distance: " + '\033[94m' + '\033[1m' + str(round(dist,2)) + "km" +  '\033[0m' + '\033[93m' + " Cooldown: " + '\033[92m' + '\033[1m' + cd + '\033[0m')
         else:
             print('\033[93m' + "» Teleporting... Distance: " + '\033[94m' + '\033[1m' + str(round(dist,2)) + "km" +  '\033[0m' + '\033[93m' + " Cooldown: " + '\033[92m' + '\033[1m' + cd + "~" + safe + '\033[0m')
 
-        # Save last coords
-        blc = lc
-        lc = [lat, lon]
+        # Save coords
+        pc = lc # save to previous
+        lc = [lat, lon] # replace latest
 #End
 
 # CALL MAIN
